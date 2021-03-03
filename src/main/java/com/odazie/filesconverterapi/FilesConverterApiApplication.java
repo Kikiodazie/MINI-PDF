75e9094a48d9a343e0ceced5dfa6ba73b6f83f0f
Kikiodazie <kiki.odazie@yahoo.com>
2021-03-03 18:20:38 +0100

    Initial commit

---
+++ src/main/java/com/odazie/filesconverterapi/FilesConverterApiApplication.java
@@ -0,0 +1,13 @@
+package com.odazie.filesconverterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesConverterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilesConverterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odazie/filesconverterapi/FilesConverterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odazie.filesconverterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesConverterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
